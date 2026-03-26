0af174fd5ff3f86d026ba0878ae028911c6d3caf
Eduard Hirsch <ehirsch@pbs.org>
2026-03-26 09:50:28 +0200

    CAT-26325 Loosen up the namespace validation to allow everything AWS/

--- required.tf
+++ required.tf
@@ -2,11 +2,7 @@ variable "namespace" {
   description = "The namespace for the alarm's associated metric. See docs for the list of namespaces. See docs for supported metrics."
   validation {
     condition = contains([
-      "AWS/Lambda",
-      "AWS/Kinesis",
-      "AWS/SQS",
-      "AWS/States",
-      "AWS/SES",
+      "AWS/",
     ], var.namespace)
     error_message = "Namespace is not currently supported by this module."
   }
